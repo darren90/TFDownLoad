ade62b9732e8d9461e77d8ee841a8a94a1306ec4
Fengtf <1005052145@qq.com>
2015-11-10 00:29:17 +0800

    重新添加ASI请求框架

---
+++ Demo/TFDownLoad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TFDownLoad
+//
+//  Created by Fengtf on 15/11/10.
+//  Copyright © 2015年 ftf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
